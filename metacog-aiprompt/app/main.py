e177a448ebd86a63feb1da20a5a6dc5d93bd8083
KaushikV0007 <kaushikveeranala@gmail.com>
2026-05-18 11:33:31 +0530

    fix: update CORS to allow Vercel preview domains using regex

--- metacog-aiprompt/app/main.py
+++ metacog-aiprompt/app/main.py
@@ -53,6 +53,7 @@ if FRONTEND_URL:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
